51d1d3e056009989cd437c9aa14f7af327d69bcd
Emil <codewithemil@gmail.com>
2022-03-12 12:46:17 -0600

    Basic styles are still being added. Almost done with the first element's styles.

---
+++ js/one.js
@@ -0,0 +1,10 @@
+let input = document.querySelector("#input-one");
+input.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        validate(e);
+    }
+});
+
+let validate = (e) => {
+    alert(`${input.value} is the current text.`);
+}
