6b871d0ca01480d55f10e9f6517bb1198bcdd161
Emil <codewithemil@gmail.com>
2022-03-12 13:08:47 -0600

    Finished styling and writing the HTML for the first command line. Hope there'll be no issues on the following ones.

--- js/one.js
+++ js/one.js
@@ -6,6 +6,11 @@ input.addEventListener("keydown", function (e) {
 });
 
 let validate = (e) => {
-    alert(`"${input.value}" is the current text.`);
+    let exTxt = "";
+
+    if (input.value === "My mom is amazing!")
+        exTxt = " Now run and give her a hug! <3";
+
+    alert(`"${input.value}" is the current text.${exTxt}`);
     input.value = null;
 }
